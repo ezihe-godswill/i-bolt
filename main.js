c7e86f4402681ba23619f8ecb04afaf0bd4678d6
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-04-18 18:11:40 +0100

    add navbar markup and styles

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 import './style.css'
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+const menuButton = document.querySelector(".menu-toggle");
+
+const toggleMenu = () => {
+  menuButton.classList.toggle("open");
+}
+
+menuButton.addEventListener("click", toggleMenu);
